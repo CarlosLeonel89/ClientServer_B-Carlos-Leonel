7e2ca380d89be52ddd26c471a6833fde7831e70b
Carlos Leonel <carlos.najera.aguilar@gmail.com>
2022-06-14 15:38:46 -0500

    new

--- routes/languages.js
+++ routes/languages.js
@@ -14,6 +14,42 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+router.post("/:id", async function(req, res, next){
+
+  try{
+      res.json(await languages.create(req.params.id, req.body))
+  }
+  catch(error){
+      console.log("Error while creating a language", error.message);
+      next(error);
+  }
+
+});
+
+router.put("/:id", async function(req, res, next){
+
+  try{
+      res.json(await languages.update(req.params.id, req.body))
+  }
+  catch(error){
+      console.log("Error while updating a language", error.message);
+      next(error);
+  }
+
+});
+
+router.delete("/:id", async function(req, res, next){
+
+  try{
+      res.json(await languages.remove(req.params.id))
+  }
+  catch(error){
+      console.log("Error while deleting a language", error.message);
+      next(error);
+  }
+
+});
+
 module.exports = router;
 
 router.post('/',async function(req,res,next){
--- services/languages.js
+++ services/languages.js
@@ -48,3 +48,5 @@ module.exports = {
     getMultiple,
     create
 }
+
+//Inestar el codigo
